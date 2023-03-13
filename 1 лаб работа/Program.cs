ba7ecd32f32eec92d67f0e2b50cc18490ae9d3df
arkadij <sadovnikovarkadyi2003@gmail.com>
2023-03-13 18:49:22 +0700

    some moew

--- 1 лаб работа/Program.cs
+++ 1 лаб работа/Program.cs
@@ -1,10 +1,7 @@
 ﻿static void Main()
 {
-    Market market = new Market();
-    for (int i = 0; i < market.allProducts.Count(); i++) 
-    {
-        Console.WriteLine($"{market.allProducts[i].Name}");
-    }
+    Cart cart = new Cart();
+    cart.take_products();
 }
 
 Main();
@@ -18,14 +15,14 @@ public class Product
 public class Market
 {
     public List<Product> Snacks = new List<Product>(new Product[] {new Product{Name="ChocolateBar", Elem="Carbohydratese"}, 
-                                                               new Product{Name="BalykCheese", Elem="Углевод"},
-                                                               new Product{Name="Crisps", Elem="Углевод"}});
-    public List<Product> semiFinishedProduct = new List<Product>(new Product[] {new Product{Name="Шоколадный ботончик", Elem="Углевод"}, 
-                                                               new Product{Name="Шоколадный ботончик", Elem="Углевод"},
-                                                               new Product{Name="Шоколадный ботончик", Elem="Углевод"}});
-    public List<Product> firtstProducts =  new List<Product>(new Product[] {new Product{Name="Шоколадный ботончик", Elem="Углевод"}, 
-                                                               new Product{Name="Шоколадный ботончик", Elem="Углевод"},
-                                                               new Product{Name="Шоколадный ботончик", Elem="Углевод"}});
+                                                               new Product{Name="BalykCheese", Elem="Proteins"},
+                                                               new Product{Name="Crisps", Elem="Fats"}});
+    public List<Product> semiFinishedProduct = new List<Product>(new Product[] {new Product{Name="Chicken", Elem="Proteins"}, 
+                                                               new Product{Name="Fruit", Elem="Carbohydratese"},
+                                                               new Product{Name="OliveOil", Elem="Fats"}});
+    public List<Product> firtstProducts =  new List<Product>(new Product[] {new Product{Name="DumplingsMeat", Elem="Proteins"}, 
+                                                               new Product{Name="DumplingsBerries", Elem="Carbohydratese"},
+                                                               new Product{Name="Cheburek", Elem="Fats"}});
     public List<Product> allProducts = new List<Product>();
     
 
@@ -41,6 +38,7 @@ public class Cart
 {
     public List<Product> cart = new List<Product>();
     public Market market = new Market();
+    public List<String> elements = new List<string>();
     
     public void take_products()
     {
@@ -48,16 +46,23 @@ public class Cart
         {
             Console.WriteLine("Введите номер товара, 0 для завершения\n");
             Console.WriteLine("Список товаров:");
-            for (int i = 0; i < market.allProducts.Count(); i++) 
+            for (int i = 0; i < this.market.allProducts.Count(); i++) 
             {
-                Console.WriteLine($"{i + 1}) {market.allProducts[i].Name}");
+                int number = i + 1;
+                Console.WriteLine($"{number.ToString()}) {this.market.allProducts[i].Name}");
             }
             int choise = Convert.ToInt32(Console.ReadLine());
             if (choise == 0)
             {
                 return;
             }
-            cart.Add(market.allProducts[choise - 1]);
+            this.cart.Add(this.market.allProducts[choise - 1]);
+            if (!this.elements.Contains(this.market.allProducts[choise - 1].Elem))
+            {
+                this.elements.Add(this.market.allProducts[choise - 1].Elem);
+            }
+            this.elements.Add(market.allProducts[choise - 1].Elem);
+            Console.WriteLine($"{market.allProducts[choise - 1].Name} добавлен!");
         }
     }
 }
